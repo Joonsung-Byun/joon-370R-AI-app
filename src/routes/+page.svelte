5a987ef148dae713b50206dfa384a67db26f32d5
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2025-01-12 13:35:58 -0700

    Add Skeleton UI components and update configurations
    - Introduced Skeleton AppBar and Lucide icons in the main page.
    - Updated package.json to include Skeleton and Lucide dependencies.
    - Modified Tailwind config to support Skeleton themes.
    - Adjusted .gitignore to include .npmrc.
    - Changed development server port in package.json.
    - Added data-theme attribute to the HTML body for theme support.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+    import { AppBar } from '@skeletonlabs/skeleton-svelte';
+    import ArrowLeft from 'lucide-svelte/icons/arrow-left';
+    import Paperclip from 'lucide-svelte/icons/paperclip';
+    import Calendar from 'lucide-svelte/icons/calendar';
+    import CircleUser from 'lucide-svelte/icons/circle-user';
+</script>
+
+<div>
+  <AppBar>
+    {#snippet lead()}
+      <ArrowLeft size={24} />
+    {/snippet}
+    {#snippet trail()}
+      <Paperclip size={20} />
+      <Calendar size={20} />
+      <CircleUser size={20} />
+    {/snippet}
+
+    {#snippet children()}
+      <h2 class="h2">AI APP</h2>
+    {/snippet}
+    
+    <!-- {#snippet headline()}
+      <h2 class="h2">Headline</h2>
+    {/snippet} -->
+  </AppBar>
+<h1 class="text-white h-screen bg-black text-center pt-20 text-2xl font-semibold">Welcome to my AI Application!</h1>
+</div>
+
+
