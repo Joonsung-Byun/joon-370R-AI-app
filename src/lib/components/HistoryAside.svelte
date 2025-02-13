c289deec7e7f270feab804ea75952a37ff3b3200
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2025-02-12 23:52:15 -0700

    Update HistoryAside component to improve placeholder text and clean up unused SVG

--- src/lib/components/HistoryAside.svelte
+++ src/lib/components/HistoryAside.svelte
@@ -30,14 +30,13 @@
  <aside id="default-sidebar" class="absolute hidden xl:block xl:relative z-20 bg-surface-600 min-h-screen h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full md:px-3 md:py-4 overflow-y-auto bg-surface-800">
        <ul class="space-y-2 font-medium">
-          <li>
-             <a href="#" class="flex items-center p-2  rounded-lg text-white group">
-                <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+          <li class="text-white">
+                <!-- <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                </svg>
-                <span class="ms-3 ">Place holder</span>
-             </a>
+                </svg> -->
+                <span class="ms-3 ">History Place holder</span>
+
           </li>      
          </ul>
     </div>
